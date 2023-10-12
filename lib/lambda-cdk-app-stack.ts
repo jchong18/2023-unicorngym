23be7ac57b2273533433d9ac7d7aac38a1a58b21
jchong18 <jhchong1018@gmail.com>
2023-10-12 14:47:53 +0900

    Creating cart service

--- lib/lambda-cdk-app-stack.ts
+++ lib/lambda-cdk-app-stack.ts
@@ -2,11 +2,13 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { OrderFunction } from './constructs/order-function';
+import { CartFunction } from './constructs/cart-function';
 
 export class LambdaCdkAppStack extends Stack {
   constructor(scope: Construct, id: string, restApi: RestApi, props?: StackProps) {
     super(scope, id, props);
 
     const Order = new OrderFunction(this, 'OrderFunction', restApi);
+    const Cart = new CartFunction(this, 'CartFunction', restApi);
   }
 }
